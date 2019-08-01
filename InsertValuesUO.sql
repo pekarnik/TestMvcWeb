1a4f39ce790d03c3bdb275f553cccfabf6721ca0
Pekarnik <pekgoglo@gmail.com>
2019-08-01 20:50:09 +0300

    Вставка случайных значений в таблицы базы UserOrders

---
+++ InsertValuesUO.sql
@@ -0,0 +1,36 @@
+Use UserOrders;
+
+
+DECLARE @i int;
+SET @i = 0
+DECLARE @dat Date;
+SET @dat=GETDATE()
+WHILE @i < 100
+BEGIN
+INSERT INTO Users
+VALUES(CONCAT('ÔÈÎ',@i),dateadd(day,-10000+@i,@dat), RAND())
+SET @i=@i+1
+end
+
+GO
+
+DECLARE @i int;
+SET @i=0
+DECLARE @dat Date;
+SET @dat=GETDATE()
+DECLARE @j int;
+SET @j=0
+WHILE @i<100
+BEGIN 
+INSERT INTO Orders
+VALUES ((SELECT MIN(ID) FROM Users)+@i,
+CONCAT('Description',Rand()*@i),
+dateadd(day,-@j,@dat),
+Rand()*100)
+SET @j=@j+1
+IF @j=15 SET @i=@i+1
+IF @j=15 SET @j=0
+
+end
+
+GO
